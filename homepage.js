8d0d04d11139133d3f35e79d0703869db2d1d19d
WindingMotor <kid.subudhi@gmail.com>
2023-08-21 21:55:25 -0400

    Better Homepage /w patterns

--- homepage.js
+++ homepage.js
@@ -2,10 +2,13 @@ var prevScrollPos = window.scrollY;
 
 window.addEventListener('scroll', function() {
   var currentScrollPos = window.scrollY;
+  var header = document.getElementById("header");
+  
   if (prevScrollPos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
+    header.style.top = "0";
   } else {
-    document.getElementById("header").style.top = "-80px"; // Adjust this value as needed
+    header.style.top = `-${header.clientHeight}px`;
   }
+  
   prevScrollPos = currentScrollPos;
 });
