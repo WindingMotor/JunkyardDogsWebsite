59f6f3d8e74ee752098a9fa6d8c87ba775bc8ad7
WindingMotor <kid.subudhi@gmail.com>
2023-08-23 14:53:17 -0400

    header fix

--- homepage.js
+++ homepage.js
@@ -4,7 +4,10 @@ window.addEventListener('scroll', function() {
   var currentScrollPos = window.scrollY;
   var header = document.getElementById("header");
   
-  if (prevScrollPos > currentScrollPos) {
+  if(currentScrollPos < 15){
+    header.style.top = "0"
+  }
+  else if(prevScrollPos > currentScrollPos) {
     header.style.top = "0";
   } else {
     header.style.top = `-${header.clientHeight}px`;
